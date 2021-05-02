8b936b588fd0411618034c33fde5547d26d35a03
Prasang <prasang023.2@gmail.com>
2021-05-02 19:32:20 +0530

    passport

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 //const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
-
+/*const md5 = require("md5");*/
+//const bcrypt = require("bcrypt");
+//const saltRounds = 10;
+const session = require('express-session');
+var passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 
 const app = express();
@@ -15,17 +19,35 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(session({
+   secret: "This is a secret",
+   resave: false,
+   saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+// mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 
 //userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("home");
@@ -36,25 +58,68 @@ app.get("/login", function(req, res){
 });
 
 app.get("/register", function(req, res){
+
+
     res.render("register");
 });
 
+app.get("/secrets", function(req, res){
+    if(req.isAuthenticated()){
+    res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+}) 
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+      if(err){
+          console.log(err);
+          res.redirect("/register");
+      }  else{
+          passport.authenticate("local")(req,res, function(){
+            res.redirect("/secrets");
+          });
+      }
+    })
+/*
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            } else{
+                res.render("secrets");
+            }
+        }); 
     });
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        } else{
-            res.render("secrets");
-        }
-    });  
+     */
 });
 
 app.post("/login", function(req, res){
-    
+     const user = new User({
+         username: req.body.username,
+         password: req.body.password
+     });
+     req.login(user, function(err){
+         if (err){
+             console.log(err);
+         } else {
+             passport.authenticate("local")(req, res, function(){
+                 res.redirect("/secrets");
+             });
+         }
+     })
+    /*
         const username =  req.body.username;
         const password =  req.body.password;
         
@@ -62,11 +127,16 @@ app.post("/login", function(req, res){
             if (err){
                 console.log(err);
             } else{
-                if(foundUser.password == password){
-                    res.render("secrets");
+                if(foundUser){
+                    bcrypt.compare(password, foundUser.password, function(err, result){
+                        if(result === true)
+                        res.render("secrets");
+                    }); 
+                    
                 }
             }
         })
+        */
 });
 
 app.listen(3000, function(){
